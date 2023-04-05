99b40f1cafa2e0e154f80181805aceccc25d99aa
vyacheslavpugovkin <128379771+vyacheslavpugovkin@users.noreply.github.com>
2023-04-05 21:05:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-function backToTop() {
-    let button = $('.back-to-top');
-
-    $(window).on('scroll', () => {
-        if ($(this).scrollTop() >= 50) {
-            button.fadeIn();
-        } else {
-            button.fadeOut();
-        }
-    });
-
-    button.on(click, (e) => {
-        e.preventDefault();
-        $('html').animate({scrollTop: 0}, 1000);
-    })
-}
-
-backToTop();
-
 let tg = window.Telegram.WebApp; 
 
 tg.expand();
